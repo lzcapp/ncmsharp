5eedafdc7abc4f3e4c59a55c5e534ba8b5262541
Laurence Luo <admin@lzc.app>
2024-08-20 23:05:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ncmsharp
+ 
---
+++ ncmsharp_cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ncmsharp_cli {
+    internal class Program {
+        static void Main(string[] args) {
+            // 文件名
+            string filePath = "test.ncm";
+
+            // 创建 NeteaseCrypt 类的实例
+            Cryption cryption = new Cryption(filePath);
+
+            // 启动转换过程
+            int result = cryption.Dump();
+
+            // 修复元数据
+            cryption.FixMetadata();
+
+            // [务必]销毁 NeteaseCrypt 类的实例
+            cryption.Destroy();
+        }
+    }
+}
