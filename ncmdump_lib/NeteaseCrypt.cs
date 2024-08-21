16395d5e38ee369f7b7e9f7430462e49ab46df60
Laurence Luo <admin@lzc.app>
2024-08-21 14:26:19 +0800

    Update

---
+++ ncmdump_lib/NeteaseCrypt.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ncmdump_lib {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public partial class NeteaseCrypt {
+        private const string DllPath = "libncmdump.dll";
+
+        [LibraryImport(DllPath, EntryPoint = "CreateNeteaseCrypt", StringMarshalling = StringMarshalling.Utf8)]
+        private static partial IntPtr CreateNeteaseCrypt(IntPtr path);
+
+        [LibraryImport(DllPath, EntryPoint = "Dump")]
+        private static partial int Dump(IntPtr NeteaseCrypt);
+
+        [LibraryImport(DllPath, EntryPoint = "FixMetadata")]
+        private static partial void FixMetadata(IntPtr NeteaseCrypt);
+
+        [LibraryImport(DllPath, EntryPoint = "DestroyNeteaseCrypt")]
+        private static partial void DestroyNeteaseCrypt(IntPtr NeteaseCrypt);
+
+        private readonly IntPtr NeteaseCryptClass;
+
+        /// <summary>
+        /// 创建 NeteaseCrypt 类的实例。
+        /// </summary>
+        /// <param name="fileName">网易云音乐 ncm 加密文件路径</param>
+        public NeteaseCrypt(string fileName) {
+            var bytes = Encoding.UTF8.GetBytes(fileName);
+
+            var inputPtr = Marshal.AllocHGlobal(bytes.Length + 1);
+            Marshal.Copy(bytes, 0, inputPtr, bytes.Length);
+            Marshal.WriteByte(inputPtr, bytes.Length, 0);
+
+            NeteaseCryptClass = CreateNeteaseCrypt(inputPtr);
+        }
+
+        /// <summary>
+        /// 启动转换过程。
+        /// </summary>
+        /// <returns>返回一个整数，指示转储过程的结果。如果成功，返回true；如果失败，返回false。</returns>
+        public bool Dump() {
+            return Dump(NeteaseCryptClass) == 0;
+        }
+
+        /// <summary>
+        /// 修复音乐文件元数据。
+        /// </summary>
+        public void FixMetadata() {
+            FixMetadata(NeteaseCryptClass);
+        }
+
+        /// <summary>
+        /// 销毁 NeteaseCrypt 类的实例。
+        /// </summary>
+        public void Destroy() {
+            DestroyNeteaseCrypt(NeteaseCryptClass);
+        }
+    }
+}
--- ncmsharp_cli/Program.cs
+++ ncmsharp_cli/Program.cs
@@ -1,20 +1,104 @@
-﻿namespace ncmsharp_cli {
-    internal class Program {
-        static void Main(string[] args) {
-            // 文件名
-            string filePath = "test.ncm";
+﻿using System.Text;
+using ncmdump_lib;
 
-            // 创建 NeteaseCrypt 类的实例
-            Cryption cryption = new Cryption(filePath);
+// ReSharper disable once IdentifierTypo
+namespace ncmsharp_cli {
+    internal static class Program {
+        private static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8;
 
-            // 启动转换过程
-            int result = cryption.Dump();
+            Console.WriteLine("Copyright \u00a9 2024 RainySummer, All Rights Reserved.");
 
-            // 修复元数据
-            cryption.FixMetadata();
+            var list = new List<string>();
 
-            // [务必]销毁 NeteaseCrypt 类的实例
-            cryption.Destroy();
+            if (args.Length == 0) {
+                string path;
+                do {
+                    Console.Write(">> ");
+                    path = Console.ReadLine() ?? string.Empty;
+                } while (string.IsNullOrWhiteSpace(path));
+                list.Add(path);
+            } else {
+                list.AddRange(args);
+            }
+
+            if (list.Count == 0) {
+                return;
+            }
+
+            try {
+                char[] trimChars = [
+                    '"', '\'', ' '
+                ];
+
+                var fileList = new List<string>();
+
+                for (var i = 0; i < list.Count; i++) {
+                    list[i] = list[i].TrimStart(trimChars).TrimEnd(trimChars).Trim();
+                    var filePath = list[i];
+                    if ((File.GetAttributes(filePath) & FileAttributes.Directory) == FileAttributes.Directory) {
+                        DirectoryInfo directoryInfo = new(filePath);
+                        fileList.AddRange(GetFiles(directoryInfo));
+                    }
+                }
+
+                if (fileList.Count > 0) {
+                    var totalCount = fileList.Count;
+                    Console.WriteLine("Totally " + totalCount + " Files.");
+                    var (_, top) = Console.GetCursorPosition();
+                    for (var index = 0; index < totalCount; index++) {
+                        var filePath = fileList[index];
+                        var status = ProcessFile(filePath);
+                        Console.SetCursorPosition(0, top);
+                        Console.Write(new string(' ', Console.WindowWidth));
+                        Console.SetCursorPosition(0, top);
+                        var percentage = index == 0 ? 0 : (double)index / totalCount * 100;
+                        if (index < totalCount - 1) {
+                            Console.Write("[" + percentage.ToString("F2") + "%] " + filePath + " " + (status ? "√" : "×"));
+                        } else {
+                            Console.Write("[100%] " + (status ? "√" : "×"));
+                        }
+                    }
+                }
+            } catch (Exception) {
+                // ignored
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key To Exit...");
+            Console.ReadKey();
+        }
+
+        private static List<string> GetFiles(DirectoryInfo dirInfo) {
+            if (dirInfo is not { Exists: true }) {
+                throw new DirectoryNotFoundException($"The directory '{dirInfo.FullName}' does not exist.");
+            }
+            List<string> fileList = [];
+            var fsInfos = dirInfo.GetFileSystemInfos();
+            foreach (FileSystemInfo fsInfo in fsInfos) {
+                if (fsInfo is DirectoryInfo subDirInfo) {
+                    fileList.AddRange(GetFiles(subDirInfo));
+                } else {
+                    if (fsInfo.Extension.Equals(".ncm", StringComparison.OrdinalIgnoreCase)) {
+                        fileList.Add(fsInfo.FullName);
+                    }
+                }
+            }
+            return fileList;
+        }
+
+        private static bool ProcessFile(string filePath) {
+            try {
+                var neteaseCrypt = new NeteaseCrypt(filePath);
+                var result = neteaseCrypt.Dump();
+                if (result) {
+                    neteaseCrypt.FixMetadata();
+                }
+                neteaseCrypt.Destroy();
+                return true;
+            } catch (Exception) {
+                return false;
+            }
         }
     }
 }
