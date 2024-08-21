502ea1b9f0159e7f5b2f5b4fef1c0f4f24107f5d
Laurence Luo <admin@lzc.app>
2024-08-21 10:42:49 +0800

    Update project reference

--- ncmsharp_cli/Program.cs
+++ ncmsharp_cli/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace ncmsharp_cli {
+﻿namespace ncmsharp_cli {
     internal class Program {
         static void Main(string[] args) {
             // 文件名
